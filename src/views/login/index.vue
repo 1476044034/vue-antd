afacf7c974be65efd7a9ded66c4c948de13a66cc
tangjun <1476044034@qq.com>
2020-12-15 15:36:37 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,116 +1,133 @@
-<!--
- * @Description: 
- * @Version: 1.0
- * @Autor: zero
- * @Date: 2020-11-14 18:40:01
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-11-27 14:01:11
--->
 <template>
-  <section class="login" ref="root">
-    <h2>{{ course }}</h2>
-    <h2 @click="addCount">{{ count }}++</h2>
-    <p type="primary" @click="login" :ref="setRef">登陆</p>
-    <a-button type="danger" @click="SET_COURSE">修改课程</a-button>
-    <Test></Test>
-  </section>
+  <div class="login_box">
+    <div class="content">
+      <h3 class="title">科技后台管理系统</h3>
+      <a-form layout="vertical" :model="form" :rules="formRule" ref="formRef">
+        <a-form-item label="用户名" name="username">
+          <a-input v-model:value="form.username" placeholder="请输入用户名">
+            <template #prefix>
+              <UserOutlined style="color:rgba(0,0,0,.25)" /></template>
+          </a-input>
+        </a-form-item>
+        <a-form-item label="密码" name="password">
+          <a-input-password v-model:value="form.password" placeholder="请输入密码">
+            <template #prefix>
+              <LockOutlined style="color:rgba(0,0,0,.25)" /></template>
+          </a-input-password>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" block @click="onSubmit">登 陆</a-button>
+        </a-form-item>
+        <a-form-item class="flex_box" style="padding:0">
+          <router-link class="link_a" to>忘记密码？</router-link>
+          <router-link class="link_a" to>注册</router-link>
+        </a-form-item>
+      </a-form>
+    </div>
+  </div>
 </template>
+
 <script lang="ts">
-import { defineComponent, computed, ref, watchEffect, onMounted, nextTick } from "vue";
+import { defineComponent, reactive, toRefs, ref } from "vue";
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+import { message } from "ant-design-vue";
 import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-import * as Types from "@/store/modules/actions-types";
-import { Course } from "@/store/modules/app";
-import { GloableState } from "@/store";
-enum Test {
-  color,
-  age,
-}
 
 export default defineComponent({
+  name: "login",
   setup() {
-    const router = useRouter();
-    const store = useStore<GloableState>();
-    const count: object = ref(0);
-    const course = computed(() => {
-      return store.state.app.course;
-    });
-    const root = ref(null);
-    watchEffect(() => {
-      console.log((count as any).value, "opopo");
-    });
-    setTimeout(() => {
-      (count as any).value = 2;
-    }, 200);
-    // 位移运算
-    enum shapsFlag {
-      ELEMENT = 1,
-      COMPONENT = 1 << 2,
-      BIG = 1 << 3,
-      SMALL = 1 << 4,
-    }
-    const vnode = {
-      shapFlag: 0,
-    };
-    const addCount = (): void => {
-      console.log(count);
-      (count as any).value++;
-    };
-    const demo = (element = shapsFlag.ELEMENT, type = shapsFlag.SMALL) => {
-      vnode.shapFlag = element |= type;
-      console.log(vnode, "vnode");
-    };
-    let myRef: HTMLElement | null = null;
-    const setRef = function (el: HTMLElement | null): void {
-      myRef = el;
-    };
-    const demo1 = (val: number) => {
-      if (val & shapsFlag.ELEMENT) {
-        console.log("element");
+    let checkUsername = async (rule: any, value: any) => {
+      console.log(value, "value");
+      if (!value) {
+        return Promise.reject("Please input the age");
+      }
+      if (!Number.isInteger(value)) {
+        return Promise.reject("Please input digits");
       } else {
-        console.log("组件");
+        if (value < 18) {
+          return Promise.reject("Age must be greater than 18");
+        } else {
+          return Promise.resolve();
+        }
       }
     };
-    const demo3 = function (params1: string) {
-      console.log(params1.length);
-    };
-    demo();
-    demo1(10);
-    demo3("str");
-    const test = function (val?: Test): void {
-      console.log(val);
-    };
-    const login = function () {
-      router.push({
-        name: "about1",
-      });
-    };
-    const SET_COURSE = function () {
-      store.commit(`app/${Types.SET_COURSE}`, Course.vue);
-      test(Test.color);
-    };
-    onMounted(() => {
-      nextTick(() => {
-        (myRef as HTMLElement).style.color = "red";
-      });
+    const router = useRouter();
+    // ref需要加类型验证，不然在获取formRef.value时会有类型的错误提示
+    const formRef = ref<any>(null);
+    //表单绑定的数据
+    let formData = reactive({
+      form: {
+        username: "",
+        password: ""
+      }
     });
-
-    return {
-      login,
-      course,
-      SET_COURSE,
-      count,
-      addCount,
-      root,
-      setRef,
+    //验证规则
+    let rules = reactive({
+      formRule: {
+        username: [{ validator: checkUsername, trigger: "blur" }],
+        password: [
+          { required: true, min: 6, message: "密码最少六位", trigger: "blur" }
+        ]
+      }
+    });
+    const onSubmit = () => {
+      formRef.value
+        .validate()
+        .then((res: object) => {
+          //验证成功时跳转首页
+          // router.push({ path: "/" });
+        })
+        .catch((res: object) => {
+          //验证失败提示错误信息
+          message.error("用户名或密码未填");
+        });
     };
+    return { ...toRefs(formData), ...toRefs(rules), onSubmit, formRef };
   },
+  components: {
+    UserOutlined,
+    LockOutlined
+  }
 });
 </script>
-<style lang="less" scoped>
-.login {
-  .name {
-    color: red;
+
+<style scoped lang="less">
+.login_box {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #283542;
+  .link_a {
+    color: #666;
+    &:hover {
+      color: #1890ff;
+    }
   }
+}
+.content {
+  width: 340px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 2px;
+  .title {
+    position: relative;
+    margin-bottom: 30px;
+    font-size: 18px;
+    text-align: center;
+    &:after {
+      content: "";
+      position: absolute;
+      bottom: -10px;
+      left: -20px;
+      width: 340px;
+      border-bottom: 1px solid #eee;
+    }
+  }
+}
+.flex_box :deep(.ant-form-item-children) {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,18 @@
-const IS_PROD = ['production', 'test'].includes(process.env.NODE_ENV)
+
 module.exports = {
-  devServer:{
-    open:true,
-    hot:true
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',  //文件路径
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devDist',  //修改打包目录
+  lintOnSave: false, //关闭语法自动检测
+  devServer: {
+    open: true
   },
-
-// chainWebpack: config => {
-//   // 修复HMR
-//   config.resolve.symlinks(true);
-
-// },
-// 　css: {
-//   // 是否使用css分离插件 ExtractTextPlugin
-//   extract: IS_PROD,
-//   // 开启 CSS source maps?
-//   sourceMap: false,
-//   // css预设器配置项
-//   loaderOptions: {
-//   },
-//   // 启用 CSS modules for all css / pre-processor files.
-//   modules: false,
-// },
-};
+  css: {
+    loaderOptions: {
+      less: {
+        globalVars: { //设置全局less变量
+          primary: '#ff6900'
+        },
+      }
+    }
+  },
+}
